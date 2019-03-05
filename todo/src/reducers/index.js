370d9b0da14cf8a10d89949abee9383a4aa80dba
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-05 12:36:25 +0000

    STRETCH: Added delete completed and delete todo

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,11 +1,23 @@
-import { ADD_TODO, TOGGLE_COMPLETE, TOGGLE_INCOMPLETE } from '../actions';
+import {
+  ADD_TODO, MARK_COMPLETE, MARK_INCOMPLETE, DELETE_COMPLETED, DELETE_TODO,
+} from '../actions';
 
 const initialState = {
-  currentId: 1,
+  currentId: 3,
   todos: [
     {
       id: 1,
       value: 'Walk the dog',
+      completed: false,
+    },
+    {
+      id: 2,
+      value: 'Do cool stuff',
+      completed: false,
+    },
+    {
+      id: 3,
+      value: 'Go on vacation',
       completed: false,
     },
   ],
@@ -14,14 +26,14 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TODO: {
-      const { newToDo } = action.payload;
+      const { newTodo } = action.payload;
       return {
         ...state,
-        todos: [...state.todos, { id: state.currentId + 1, value: newToDo, completed: false }],
+        todos: [...state.todos, { id: state.currentId + 1, value: newTodo, completed: false }],
         currentId: state.currentId + 1,
       };
     }
-    case TOGGLE_COMPLETE: {
+    case MARK_COMPLETE: {
       const { id } = action.payload;
       const updatedTodos = state.todos.map((todo) => {
         if (todo.id === id) {
@@ -35,7 +47,7 @@ const reducer = (state = initialState, action) => {
       });
       return { ...state, todos: updatedTodos };
     }
-    case TOGGLE_INCOMPLETE: {
+    case MARK_INCOMPLETE: {
       const { id } = action.payload;
       const updatedTodos = state.todos.map((todo) => {
         if (todo.id === id) {
@@ -48,6 +60,15 @@ const reducer = (state = initialState, action) => {
         return todo;
       });
       return { ...state, todos: updatedTodos };
+    }
+    case DELETE_COMPLETED: {
+      const incompleteTodos = state.todos.filter(todo => !todo.completed);
+      return { ...state, todos: incompleteTodos };
+    }
+    case DELETE_TODO: {
+      const { id } = action.payload;
+      const newTodos = state.todos.filter(todo => todo.id !== id);
+      return { ...state, todos: newTodos };
     }
     default:
       return state;
