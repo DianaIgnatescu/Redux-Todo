1aa5f5a33927d3a64f5d9093c00447db60a77ceb
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-05 12:38:14 +0000

    MVP: Added action types and action creators

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -1,23 +1,39 @@
 export const ADD_TODO = 'ADD_TODO';
-export const TOGGLE_COMPLETE = 'TOGGLE_COMPLETE';
-export const TOGGLE_INCOMPLETE = 'TOGGLE_INCOMPLETE';
+export const MARK_COMPLETE = 'MARK_COMPLETE';
+export const MARK_INCOMPLETE = 'MARK_INCOMPLETE';
+export const DELETE_COMPLETED = 'DELETE_COMPLETED';
+export const DELETE_TODO = 'DELETE_TODO';
 
-export const addToDo = newToDo => ({
+export const addTodo = newTodo => ({
   type: ADD_TODO,
   payload: {
-    newToDo,
+    newTodo,
   },
 });
 
-export const toggleComplete = id => ({
-  type: TOGGLE_COMPLETE,
+export const markComplete = id => ({
+  type: MARK_COMPLETE,
   payload: {
     id,
   },
 });
 
-export const toggleIncomplete = id => ({
-  type: TOGGLE_INCOMPLETE,
+export const markIncomplete = id => ({
+  type: MARK_INCOMPLETE,
+  payload: {
+    id,
+  },
+});
+
+export const deleteCompleted = id => ({
+  type: DELETE_COMPLETED,
+  payload: {
+    id,
+  },
+});
+
+export const deleteTodo = id => ({
+  type: DELETE_TODO,
   payload: {
     id,
   },
